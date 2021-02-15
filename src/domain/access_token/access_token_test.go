90f37f0756fb5790ae7b662d0aa7f7d6432a12fe
Armingodiz <armingodarzi1380@gmail.com>
2021-02-15 19:55:09 +0330

    using assert in GetNewAccessToken for testing as a practice

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -1,23 +1,16 @@
 package access_token
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
 func TestGetNewAccessToken(t *testing.T) {
 	at := GetNewAccessToken()
-	if at.IsExpired() {
-		t.Error("new access token should not be expired !")
-
-	}
-	if at.AccessToken != "" {
-		t.Error("new access token should have empty access token value ")
-
-	}
-	if at.UserID != 0 {
-		t.Error("new access token cant have user id ")
-	}
+	assert.False(t, at.IsExpired(), "new access token should not be expired !")
+	assert.EqualValues(t, "", at.AccessToken, "new access token should have empty access token value ")
+	assert.True(t, at.UserID == 0, "new access token cant have user id ")
 }
 func TestAccessToken_IsExpired(t *testing.T) {
 	at := GetNewAccessToken()
