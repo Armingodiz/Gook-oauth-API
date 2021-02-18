3952a1b5ef3624f1ede9ffba241c2cb7cf6dd210
Armingodiz <armingodarzi1380@gmail.com>
2021-02-18 14:53:41 +0330

    LoginUser method for userRestRepository added

--- src/app/application.go
+++ src/app/application.go
@@ -15,5 +15,5 @@ func StartApplication() {
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 	router.POST("/oauth/access_token", atHandler.Create)
-	router.Run(":8080")
+	router.Run(":2222")
 }
--- src/repository/rest/users_repository.go
+++ src/repository/rest/users_repository.go
@@ -1,8 +1,11 @@
 package rest
 
 import (
+	"encoding/json"
 	"github.com/ArminGodiz/Gook-oauth-API/src/domain/users"
 	"github.com/ArminGodiz/Gook-oauth-API/src/utils/errors"
+	"github.com/golang-restclient/rest"
+	"time"
 )
 
 type RestUsersRepository interface {
@@ -11,10 +14,33 @@ type RestUsersRepository interface {
 type restUsersRepository struct {
 }
 
+var (
+	UsersRestClient = rest.RequestBuilder{
+		BaseURL: "localhost:1111", // ** the port which users api is listening on **
+		Timeout: 2 * time.Second,
+	}
+)
+
 func NewRepository() RestUsersRepository {
 	return &restUsersRepository{}
 }
 
 func (ur *restUsersRepository) LoginUser(email, password string) (*users.User, *errors.RestErr) {
-
+	response := UsersRestClient.Post("/users/login", users.LoginRequest{Email: email, Password: password})
+	if response == nil || response.Response == nil {
+		return nil, errors.NewInternalServerError("INVALID email or password for login !")
+	}
+	if response.StatusCode > 299 { // we have an error
+		var restErr errors.RestErr
+		err := json.Unmarshal(response.Bytes(), &restErr)
+		if err != nil { // we get a different type of error
+			return nil, errors.NewInternalServerError("Unknown error type accrued while trying to login ")
+		}
+		return nil, &restErr
+	}
+	var user users.User
+	if err := json.Unmarshal(response.Bytes(), &user); err != nil {
+		return nil, errors.NewInternalServerError("error in unmarshalling response ")
+	}
+	return &user, nil
 }
